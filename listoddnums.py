810c1be4090add0a7a165a6bb6ee377bc07e9d43
Juli <143529579+Julissa-Paramo@users.noreply.github.com>
2023-10-09 19:57:28 -0500

    Create listoddnums.py

---
+++ listoddnums.py
@@ -0,0 +1,15 @@
+'''
+Julissa Paramo
+10 / 9 / 23
+Filtering a list
+'''
+
+# given a list, filter out the even numbers and give a list of off numbers
+
+nums = input()
+
+numlist = nums.split
+
+for i in numlist:
+
+  
