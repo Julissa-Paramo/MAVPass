ff0f1d284be5652396438cb173b7bce5c16be098
Juli <143529579+Julissa-Paramo@users.noreply.github.com>
2023-10-16 16:36:52 -0500

    Update functions.max.py

--- functions.max.py
+++ functions.max.py
@@ -7,25 +7,25 @@ Function for finding the Max
 # functions
 # lists
 
-def find_max(listA):
+def find_max(listA): # function name and parameters
 
-  max = listA[0]
+  max = listA[0] # assigns the first element in the list as the max automatically
   
-  if len(listA) == 0:
+  if len(listA) == 0: # if the length is 0, prints an error
 
     print('empty list')
 
-  else:
+  else: # if there is data in the list
   
-    for num in listA:
+    for num in listA: # iterates through all the numbers in the list
     
-        if num > max:
+        if num > max: # if number is greater than the previosly assigned num
 
-            max = num
+            max = num # that num will be the new max
 
-    return max
+    return max # returns the max
 
 
-numlist = [-1,-2,0,-3]
+numlist = [-1,-2,0,-3] # function in action
 
-print(find_max(numlist))
+print(find_max(numlist)) # returns 0
