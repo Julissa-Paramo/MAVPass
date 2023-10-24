870169037146101a047bc3d0c1aa6853e60ce45a
Juli <143529579+Julissa-Paramo@users.noreply.github.com>
2023-10-23 19:36:21 -0500

    Create populationdictionary.py

---
+++ populationdictionary.py
@@ -0,0 +1,22 @@
+'''
+Julissa Paramo
+10 / 23 / 23
+Population addition
+'''
+
+# dictionaries
+# values in dictionaries
+# for loops
+
+pop_dict = {'New York':100,'Minnesota':200,'Mississippi':300,'Vermont':400}
+
+pop_sum = 0 # counter for the sum of values
+
+for value in pop_dict.values(): # gets the values from the dictionary
+  
+  pop_sum += value # adds each value to the sum
+  
+print(pop_sum) # result is 1000 for the total population
+
+
+  
