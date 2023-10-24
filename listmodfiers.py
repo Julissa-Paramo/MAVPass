475923a5005b671fb1f51ef2c319bfb40c582ab7
Juli <143529579+Julissa-Paramo@users.noreply.github.com>
2023-10-23 19:41:21 -0500

    Update listmodfiers.py

--- listmodfiers.py
+++ listmodfiers.py
@@ -8,7 +8,7 @@ nums = [1,2,3,4,5]
 
 popped_element = nums.pop() # returns the last element in the list
 
-
+# removing duplicates from a list and making a new one
 
 num_list = [1,1,2,3,4,5,3,3,8,4]
 
